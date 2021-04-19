aff05bdb86d6fc46f4d94ae479fcc281b300ac32
winniepukki <4bogaa25@solent.ac.uk>
2021-04-19 10:46:05 +0300

    Shipped event setup with mongoDB

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+
+const Event = require('./models/event');
+
 const PORT = 3000;
 
 const app = express();
-
-const events = [];
 
 app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
@@ -39,7 +41,14 @@ app.use('/graphql', graphqlHTTP({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event.find()
+            .then((events) => {
+                return events.map((event) => {
+                    return { ...event._doc, _id: event.id }
+                })
+            }).catch((err) => {
+                throw err;
+            })
         },
         createEvent: (args) => {
             const {
@@ -51,19 +60,28 @@ app.use('/graphql', graphqlHTTP({
                 } = {}
             } = args;
 
-            const event = {
-                _id: Math.random().toString(),
+            const event = new Event({
                 title: sTitle,
                 description: sDescription,
-                price: +sPrice,
-                date: sDate
-            }
+                price: sPrice,
+                date: new Date(sDate)
+            });
 
-            events.push(event);
-            return event;
+            return event
+            .save()
+            .then((result) => {
+                console.log(result);
+                return { ...result._doc, _id: result.id };
+            })
+            .catch((err) => {
+                console.log(err);
+                throw err;
+            });
         }
     },
     graphiql: true
 }));
 
-app.listen(3000);
+mongoose
+.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@mysuperheroes.ucexn.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+.then(() => app.listen(3000));
